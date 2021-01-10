4504cf5ecefa7a061d265f10108e80d069e2ffe9
Tullio Pizzuti <tullio.pizz@gmail.com>
2021-01-10 12:12:14 +0100

    duplicate enemies

--- Assets/Scripts/InitEnemy.cs
+++ Assets/Scripts/InitEnemy.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class InitEnemy : MonoBehaviour
 {
     private Transform enemyHolder;
-    private Transform[] initialEnemies;
+    private List<Transform> initialEnemies;
+    public int[] xPosition = { -4, -3, -2, -1, 1, 2, 3, 4 };
     // Start is called before the first frame update
     void Awake()
     {
-       enemyHolder = GetComponent<Transform> ();
-       initialEnemies = GetComponents<Transform> ();
+        initialEnemies = new List<Transform>();
+        enemyHolder = GetComponent<Transform>();
+        foreach (Transform enemy in enemyHolder)
+        {
+            initialEnemies.Add(enemy);
+        }
+        foreach (Transform enemy in initialEnemies)
+        {
+            foreach (int x in xPosition)
+            {
+                Transform clone = Instantiate(enemy,
+                    new Vector3(x, enemy.position.y, enemy.position.z),
+                    enemy.rotation,
+                    enemyHolder
+                    );
+                clone.transform.parent = enemyHolder;
+            }
+
+        }
     }
 
-   
+
 }
